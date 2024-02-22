c03b78f8b0a21108241429079d8f469e95375610
santacrab2 <79347566+santacrab2@users.noreply.github.com>
2024-02-22 12:11:13 -0500

    Update vi_spawn_event.py
    asks for a species to search for and will skip over anything that spawns that isnt that species
    prints more info when it finds a shiny

--- examples/vi_spawn_event.py
+++ examples/vi_spawn_event.py
@@ -19,33 +19,34 @@ def overworld_spawn_event(gdbprocess: GdbProcess, bkpt: Breakpoint):
 
     pokemon_addr = gdbprocess.read_register("sp") + 0x18
 
-    species = Species(gdbprocess.read_int(pokemon_addr + 0x18, "h"))
+    species = gdbprocess.read_int(pokemon_addr + 0x18, "h")
+    if Species(species) != SearchSpecies:
+        print(f"{Species(species)}")
+        return False
     pid = gdbprocess.read_int(pokemon_addr + 0x8)
     tidsid = gdbprocess.read_int(pokemon_addr + 0x10)
-    if(is_shiny(pid,tidsid)):
-        print(
-            f"{species=}",
-            f"shiny = true",
-            sep="\n",
-            end="\n\n",
-        )
+    shiny = is_shiny(pid,tidsid)
+    if shiny:
+        level = gdbprocess.read_int(pokemon_addr + 0x1e, "h")
+        scale = gdbprocess.read_int(pokemon_addr + 0x4a, "b")
+        form = gdbprocess.read_int(pokemon_addr + 0x1a, "w")
+        print(f"{Species(species)}-{form} {level=} {scale=} {shiny=}")
         wait = input("press enter to continue")
+        return True
     else:
-        print(
-            f"{species=}",
-            f"shiny = false")
-
-
-
+        print(f"{Species(species)}")
+        return False
+        
 # IP of switch
 gdb_process = GdbProcess("192.168.1.151")
+SearchSpecies = Species(int(input(f"Enter a species number to search for\n")))
+print(f"searching for: {SearchSpecies}")
 # create breakpoint at address 7100D0AA60
 # (near end of pokemon generation function in Violet)
 gdb_process.add_breakpoint(Breakpoint(
-    0x7100def960,
+    0x7100def950,
     "Pokemon Generated",
-    on_break = overworld_spawn_event
-    ))
+    on_break = overworld_spawn_event))
 # connecting with gdb automatically pauses execution, resume in order to wait for breakpoints
 gdb_process.resume_execution()
 # start loop of waiting for breaks
